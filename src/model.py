8f3d2bc903628da2b47b327f543629a4b8d16fd4
Nam <phuongnam.infl@gmail.com>
2021-05-10 11:39:02 +0700

    update

--- src/model.py
+++ src/model.py
@@ -19,6 +19,7 @@ model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 model.add(Flatten())
+model.add(Dense(128, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy',
